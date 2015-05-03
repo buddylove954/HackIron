f263f7fd8f19e8c4a1b3464aea65cf694a4f811b
spinkyt <resnickmicah@gmail.com>
2015-05-03 01:32:11 -0400

    Added status enum to attends

--- app/models/attend.rb
+++ app/models/attend.rb
@@ -1,4 +1,6 @@
 class Attend < ActiveRecord::Base
   belongs_to :guest
   belongs_to :event
+
+  enum status: [:invited, :attending, :registered]
 end
--- db/migrate/20150503012131_create_attends.rb
+++ db/migrate/20150503012131_create_attends.rb
@@ -3,7 +3,7 @@ class CreateAttends < ActiveRecord::Migration
     create_table :attends do |t|
       t.belongs_to :guest, index: true
       t.belongs_to :event, index: true
-      t.string :check_in_status, null: false
+      t.integer :status, null: false
       t.timestamps null: false
     end
   end
